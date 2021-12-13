f01ca34592391b3488e226dbd5f29706ba8ec372
YudakovVV <58217922+YudakovVV@users.noreply.github.com>
2021-12-13 15:04:03 +0400

    Готовая главная страница

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@ $(document).ready(function () {
     var floorPath = $(".home-image path");//каждый отдельный этаж в SVG
     var counterUp = $(".counter-up");/*кнопка вверх*/
     var counterDown = $(".counter-down");
+    var modal = $(".modal");
+    var modalCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
 
     //функция при наведение мышки на этаж
     floorPath.on("mouseover",function(){
         floorPath.removeClass("current-floor"); /*обнуление счетчика*/
         currentFloor = $(this).attr("data-floor");/*получение значения этажа*/
         $(".counter").text(currentFloor);//записываем значениие этажа в счетчик справа
-    })
+    });
+
+    floorPath.on("click", toggleModle);//при клике на этаж вызвать окно
+    modalCloseButton.on("click", toggleModle);//при клике на кнопку закрыть закрывает окно
+    viewFlatsButton.on("click", toggleModle);//при нажатии на кнопку открывать окно
+
+
     //функция отслеживания клика по кнопке вверх
     counterUp.on("click", function(){
         if(currentFloor<18){ //проверяем значение этажа
@@ -20,7 +29,7 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");    
             $(`[data-floor=${usCurrenFloor}]`).toggleClass("current-floor"); //подсвечиваем текущий этаж /*так же чтобы работала кончтрукция ${usCurrenFloor} нужно поставить обратный апостраф*/
         }
-    })
+    });
     //функция отслеживания клика по кнопке вниз
     counterDown.on("click", function(){
         if(currentFloor>2){
@@ -31,5 +40,8 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");    
             $(`[data-floor=${usCurrenFloor}]`).toggleClass("current-floor"); /*чтобы работала конструкция ${usCurrenFloor} нужно поставить обратный апостраф*/
         }
-    })
+    }); 
+    function toggleModle(){
+        //функция открыть закрыть окно
+        modal.toggleClass("is-open")};
 });
